e61729d686e8720721612275208b0a2f600aa2b9
covid2024 <orangehere267@gmail.com>
2025-01-05 14:22:43 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,22 +2,22 @@
 const config = {
   gridRows: 14,
   gridCols: 14,
-  hospitalCost: 2000,
-  labCost: 2500,
+  hospitalCost: 200, // Giảm từ 2000 xuống 200
+  labCost: 200, // Giảm từ 2500 xuống 200
   dayDuration: 10 * 60 * 1000,
   initialMutationIn: [10, 15],
   mutationInterval: [5, 10],
   researchRate: 0.05,
   recoveryRate: 0.03,
-  hospitalRecoveryBonus: 0.005, // Tăng tỷ lệ hồi phục khi có bệnh viện
-  hospitalLocalEffectRadius: 2, // Bán kính ảnh hưởng của bệnh viện
+  hospitalRecoveryBonus: 0.005,
+  hospitalLocalEffectRadius: 2,
   deathRate: 0.01,
   researchImpactOnDeathRate: 1000,
   spreadRate: 0.3,
   spreadRadius: 3,
   chanceOfSpreading: 0.5,
-  labLocalEffectRadius: 2, // Bán kính ảnh hưởng của phòng lab
-  labLocalSpreadReduction: 0.1, // Giảm tỷ lệ lây lan khi có phòng lab
+  labLocalEffectRadius: 2,
+  labLocalSpreadReduction: 0.1,
   coinPerDay: 500,
   labUpgradeCost: 5000,
   labUpgradeResearchBonus: 0.05,
